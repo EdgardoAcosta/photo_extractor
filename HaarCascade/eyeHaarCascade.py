d5f18a94f13f8cef1055c666f8114968ddefea2a
Oberyn18 <carlosab1802@gmail.com>
2017-06-12 17:15:12 -0300

    El nuevo archivo permite seleccionar solo las fotos donde se distingan los dos ojos, es decir las más nítidas.

--- HaarCascade/eyeHaarCascade.py
+++ HaarCascade/eyeHaarCascade.py
@@ -1,25 +1,23 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
-
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml');
+img = cv2.imread('cropped_image_9.png')
 
-cap = cv2.VideoCapture(0)
-
-counter = 0
 while True:
-    ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for(x, y, w, h) in faces:
-        # Entre 59 y 65 es el rango de h y w para cortar
-        cv2.rectangle(img, (x-30, y-33), (x+w+30, y+h+70), (255, 0, 0), 2) 
+    eyes = eye_cascade.detectMultiScale(gray)
+    for(ex, ey, ew, eh) in eyes:
+        cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (255, 0, 0), 2)
+        print("ex: ", ex)
+        print("ey: ", ey)
+        print("eh: ", eh)
+        print("ew: ", ew)
         '''
         print("W: ", w)
         print("H: ", h)
         print("\n")
-        '''
+        
         # Probaré cortando la imagen ahora:
         
         if counter < 20 and h >= 83 and h <= 90 and w >= 83 and w <= 90:
@@ -33,10 +31,9 @@ while True:
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for(ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+        '''
     cv2.imshow('Webcam Scanner', img)
-    k = cv2.waitKey(30) & 0xff
-    if k == 27 or counter >= 20:
-        break;
+    cv2.waitKey(0)
+    break
 
-cap.release();
 cv2.destroyAllWindows()
